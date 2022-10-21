ff83cbac6e1a934718df12c09b3dcba96b2a00de
Bryan Albert <bryan2j@gmail.com>
2022-10-21 14:01:29 -0600

    Update contract

--- contracts/Tambora.sol
+++ contracts/Tambora.sol
@@ -12,6 +12,13 @@ contract Tambora is ERC721 {
 		_;
 	}
 
+	struct Request {
+		string name;
+		address addr;
+		bool isApproved;
+		uint256 minted; 
+	}
+
 	address payable private _owner;
 	string private _baseURIextended;
 	uint256 public tokenId;	
@@ -19,6 +26,9 @@ contract Tambora is ERC721 {
 	string public contractType;
 	mapping (uint256 => string) private _tokenURIs;
 	mapping (address => uint256[]) private _ownedTokens;
+	mapping (address => bool) private _isApproved;
+	Request[] private _approvedRequests;
+	Request[] private _approvalRequests;
 
 	constructor(address deployer, string memory name, string memory symbol, uint256 price_, string memory type_, address to_, string memory uri_) ERC721(name, symbol) {
 		tokenId = 0;
@@ -79,5 +89,40 @@ contract Tambora is ERC721 {
 
 	function _msgValue() internal view returns (uint256) {
 		return msg.value;
+	}
+
+	function getApprovalRequests() public view onlyOwner returns (Request[] memory) {
+		return _approvalRequests;
+	}
+
+	function getApprovedRequests() public view returns (Request[] memory) {
+		return _approvedRequests;
+	}
+
+	function requestApproval(string memory name) public {
+		_approvalRequests.push(Request({name: name, addr: _msgSender(), isApproved: false, minted: 0}));
+	}
+
+	function approveOrDenyRequest(uint256 index, bool decision) public onlyOwner {
+		if (decision == true) {
+			_approvalRequests[index].isApproved = true;
+			_isApproved[_approvalRequests[index].addr] = true;
+			_approvedRequests.push(_approvalRequests[index]);
+			_approvalRequests[index] = _approvalRequests[_approvalRequests.length - 1];
+			_approvalRequests.pop();
+		} else {
+			_approvalRequests[index] = _approvalRequests[_approvalRequests.length - 1];
+			_approvalRequests.pop();
+		}
+	}
+
+	function finalizeRequest(string memory uri) public payable {
+		require(_isApproved[_msgSender()] == true);
+		mint(_msgSender(), uri);
+		_owner.transfer(_msgValue());
+	}
+	
+	function isApproved() public view returns (bool) {
+		return _isApproved[_msgSender()];
 	}
 }
