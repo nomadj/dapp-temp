0979cd68c7fb1761cf8b1018a3f95e44804b1b7a
Bryan Albert <bryan2j@gmail.com>
2023-01-03 18:14:37 -0800

    Return to null render.

--- pages/[cont].js
+++ pages/[cont].js
@@ -180,15 +180,7 @@ class CampaignShow extends Component {
 
   render() {
     if (this.state.renderPage === false) {
-      return (
-	<Layout>
-	  <h1>Welcome to the future. Start
-	    <Link href='https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en'>
-	      <a> here.</a>
-	    </Link>
-	  </h1>
-	</Layout>
-      );	
+      return null;
     } else if (typeof window === 'undefined' || typeof window.web3 === 'undefined' || this.state.unsupported) {
       return (
 	<Layout>
