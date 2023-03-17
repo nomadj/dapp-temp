2703f1d67e42a2ba292bd34994947ea8ac4a280f
Bryan Albert <bryan2j@gmail.com>
2023-03-17 11:13:46 -0700

    add try/catch for brave browser

--- web3.js
+++ web3.js
@@ -4,9 +4,15 @@ let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running
+  try {
   web3 = new Web3(window.ethereum);
   // window.ethereum.enable();
-  window.ethereum.request({ method: 'eth_requestAccounts' });
+    window.ethereum.request({ method: 'eth_requestAccounts' });
+  } catch(error) {
+    web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
+    console.log(error);
+  }
 } else {
     // We are on the server or user not running metamask
     const provider = new Web3.providers.HttpProvider(process.env.INFURA_NODE_GOERLI);
