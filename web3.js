5fb2c8c4599439cad2840d34b9a5c9bf9fffe87a
Bryan Albert <bryan2j@gmail.com>
2023-03-18 12:42:48 -0700

    Add access tracker

--- web3.js
+++ web3.js
@@ -4,17 +4,13 @@ let web3;
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   // We are in the browser and metamask is running
-  try {
   web3 = new Web3(window.ethereum);
   // window.ethereum.enable();
-    window.ethereum.request({ method: 'eth_requestAccounts' });
-  } catch(error) {
-    console.log(error);
-  }
+  window.ethereum.request({ method: 'eth_requestAccounts' });
 } else {
     // We are on the server or user not running metamask
-    const provider = new Web3.providers.HttpProvider(process.env.INFURA_NODE_GOERLI);
-    web3 = new Web3(provider);
+  const provider = new Web3.providers.HttpProvider(process.env.INFURA_NODE_GOERLI);
+  web3 = new Web3(provider);
 }
 
 export default web3;
