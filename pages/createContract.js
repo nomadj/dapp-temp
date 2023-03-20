0afa9956c9f5ed0895a003dc2927c28667b011eb
Bryan Albert <bryan2j@gmail.com>
2023-03-20 00:15:24 -0700

    Add try/catch

--- pages/createContract.js
+++ pages/createContract.js
@@ -144,15 +144,15 @@ class CreateContract extends Component {
 	}
       }
       const accounts = await web3.eth.getAccounts();
-      await web3.eth.getBalance(accounts[0], (error, balance) => {
-	if (error) {
-	  throw { message: error.message };
-	}
+      try {
+	const balance = await web3.eth.getBalance(accounts[0]);
 	const bal = web3.utils.fromWei(balance, 'ether');
 	if (parseFloat(bal) < 0.05) {
-	  throw { message: 'Insufficient balance. 0.05 ETH is required to initiate this transaction.' };
+          throw { message: 'Insufficient balance. 0.05 ETH is required to initiate this transaction.' };
 	}
-      });      
+      } catch (error) {
+	throw { message: error.message };
+      }
       const tx = await factory.methods.deployTambora(this.state.name, this.state.symbol, web3.utils.toWei(this.state.price, 'ether'), this.state.contractType, accounts[0], `ipfs://${this.state.uri}`).send({from: accounts[0], value: web3.utils.toWei('0.05') });
       this.setState({ infoMessage: '', success: true, loading: false, contractAddress: tx.events['Deployed'].returnValues.contractAddr });
       async function pusher() {
